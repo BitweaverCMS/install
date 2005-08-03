9ad0211c06c9e10bd83e7eacc2593fdc627c0698
Lester Caine <lester@lsces.co.uk>
2005-08-03 16:54:00 +0000

    Clears all errors reported in phpdoc build Clean build is up on http://home.lsces.co.uk/bitweaverdocsPHP/

--- pump_bitcommerce_inc.php
+++ pump_bitcommerce_inc.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_install/Attic/pump_bitcommerce_inc.php,v 1.1.2.2 2005/08/03 16:54:00 lsces Exp $
+ * @package install
+ * @subpackage pumps
+ */
 
+/**
+ * Required files
+ */
 	require_once( BITCOMMERCE_PKG_PATH.'includes/common_inc.php' );
 	reset_bitcommerce_layout();
 
