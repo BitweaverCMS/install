fbb20400f62083ae6813370cc13d130daa7163c0
bitweaver.org <bitweaver@users.sourceforge.net>
2005-12-18 08:14:51 +0000

    modify build script to include the  languages export script

--- bitweaver_build.sh
+++ bitweaver_build.sh
@@ -4,7 +4,11 @@
 #	exit 1
 export CVS_RSH=ssh
 
-if [ -z $1 ] ; then
+if [ $1 = '--help' ] ; then
+	echo "Usage: $0 [module [BRANCHNAME [sfuser [builddir]]]]"
+	echo "(Note: If sfuser is not supplied, 'bitweaver@cvs.sf.net' will be assumed )"
+	exit 1
+elif [ -z $1 ] ; then
 	MODULE="bitweaver"
 else
 	MODULE=$1
@@ -17,15 +21,15 @@ else
 fi
 
 if [ -z $3 ] ; then
-    SFUSER=bitweaver
+	SFUSER=bitweaver
 else
-    SFUSER=$3
+	SFUSER=$3
 fi
 
 if [ -z $4 ] ; then
-    BUILDDIR=bitweaver
+	BUILDDIR=$1
 else
-    BUILDDIR=$4
+	BUILDDIR=$4
 fi
 
 
@@ -36,17 +40,25 @@ mkdir -p "/tmp/$fname/"
 cd "/tmp/$fname/"
 cvs -qz5 -d :ext:$SFUSER@cvs.sf.net:/cvsroot/bitweaver/ export -d $BUILDDIR $BRANCH $MODULE
 cd "/tmp/$fname/"
-mv "$BUILDDIR/favicon.ico.sample" "$BUILDDIR/favicon.ico"
-if [ -d "$BUILDDIR/phpbb" ] ; then
-	touch "$BUILDDIR/phpbb/config.php"
-fi
-if [ -d "$BUILDDIR/kernel" ] ; then
-	touch "$BUILDDIR/kernel/config_inc.php"
-fi
-if [ -d "$BUILDDIR/themes" ] ; then
-	mkdir "$BUILDDIR/themes/force"
-fi
 
+if [ $MODULE = 'bitweaver' ] ; then
+	mv "$BUILDDIR/favicon.ico.sample" "$BUILDDIR/favicon.ico"
+	if [ -d "$BUILDDIR/phpbb" ] ; then
+		touch "$BUILDDIR/phpbb/config.php"
+	fi
+	if [ -d "$BUILDDIR/kernel" ] ; then
+		touch "$BUILDDIR/kernel/config_inc.php"
+	fi
+	if [ -d "$BUILDDIR/themes" ] ; then
+		mkdir "$BUILDDIR/themes/force"
+	fi
+	if [ -d "$BUILDDIR/languages" ] ; then
+		# export latest language files
+		php -d display_errors=Off ~/bin/export_all_languages.php
+		cp -ru ~/live/temp/lang/export/* "$BUILDDIR/languages/lang/"
+		rm -rf ~/live/temp/lang/export/
+	fi
+fi
 
 tar -czf $fname.tar.gz $BUILDDIR
 mv $fname.tar.gz $HOME/$fname.tar.gz
