a71d0207c347a42324924fccd83101bb54ab07b8
wjames5 <will@tekimaki.com>
2009-10-01 14:17:00 +0000

    remove ref to non-existant copyright.txt file

--- install_packages.php
+++ install_packages.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_install/install_packages.php,v 1.86 2009/10/01 13:45:41 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_install/install_packages.php,v 1.87 2009/10/01 14:17:00 wjames5 Exp $
  * @package install
  * @subpackage functions
  *
  * @copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  */
 
